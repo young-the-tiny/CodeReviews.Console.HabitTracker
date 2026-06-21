8b79eb87da5cbddda55b3a6ad331fc925991233c
madme <tatrantuyen@gmail.com>
2026-06-21 15:15:49 +0700

    database

--- Program.cs
+++ Program.cs
@@ -1,1 +1,19 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Microsoft.Data.Sqlite;
+
+string connectionString = @"Data Source=habit-Tracker.db";
+
+using (var connection = new SqliteConnection(connectionString))
+{
+    connection.Open();
+    var command = connection.CreateCommand();
+    command.CommandText =
+    @"
+        CREATE TABLE IF NOT EXISTS drinking_water (
+            Id INTEGER PRIMARY KEY AUTOINCREMENT,
+            Date TEXT NOT NULL,
+            Quantity INTEGER NOT NULL
+        );
+    ";
+    command.ExecuteNonQuery();
+    connection.Close();
+}
